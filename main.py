122ecc6c1b2521eae536914e196a47441fcaf447
Ilnar Isakov <114918408+SLYPR1NC3@users.noreply.github.com>
2023-02-10 18:53:49 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import logging
+
+
+from aiogram import Bot, Dispatcher, executor, types
+
+
+API_TOKEN = '5925222028:AAFHa0Q4dVlDDUpVT864setEpQ5syMqzAh8'
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+help_command = ""
+
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.answer("Привет! Это тестовый бот!")
+
+
+@dp.message_handler(commands=['help'])
+async def help_command(message: types.Message):
+    await message.answer(text=help_command)
+    await message.delete()
+
+
+#Эхо сообщения.
+# @dp.message_handler()
+# async def echo(message: types.Message):
+#     await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+
